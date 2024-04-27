b30d87795339398d713db38b8822b7e13e7279df
muendokennedy <kennedymunyao999@gmai.com>
2024-04-27 22:57:50 +0300

    Styled the card container in the app vue to give out a good appearance of the main container

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nostrum exercitationem omnis, minus nisi corporis!</p>
+          <p class="date">May 15th 2024</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main{
+  height: 100vh;
+  width: 100vw;
+}
+.container{
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+  }
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1{
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+button{
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  color: white;
+  background: black;
+  border-radius: 100%;
+  font-size: 20px;
+  }
+  .card{
+    width: 225px;
+    height: 225px;
+    background: rgb(237, 182, 44);
+    padding: 10px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+  .date{
+    font-size: 12.5px;
+    font-weight: bold;
+  }
+</style>
